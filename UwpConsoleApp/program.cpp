2cdfe7b12910b575d000cc39f027e46ec5f8c3f3
Robson <rp.pontin@gmail.com>
2023-08-06 12:38:16 -0700

    Create a sample console UWP app

---
+++ UwpConsoleApp/program.cpp
@@ -0,0 +1,19 @@
+#include "pch.h"
+
+using namespace winrt;
+
+int main()
+{
+    // You can get parsed command-line arguments from the CRT globals.
+    wprintf(L"Parsed command-line arguments:\n");
+    for (int i = 0; i < __argc; i++)
+    {
+        wprintf(L"__argv[%d] = %S\n", i, __argv[i]);
+    }
+
+    // Keep the console window alive in case you want to see console output when running from within Visual Studio
+    wprintf(L"Press 'Enter' to continue: ");
+    getchar();
+
+    return 0;
+}
