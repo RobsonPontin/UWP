a88fbcdbbf99d2fae9e708ee63eaeefe92195f36
Robson <rp.pontin@gmail.com>
2023-09-29 09:22:28 -0700

    added simple image view code

--- MultiThreadApp/ViewPage.xaml.cs
+++ MultiThreadApp/ViewPage.xaml.cs
@@ -5,12 +5,15 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
@@ -25,6 +28,26 @@ namespace MultiThreadApp
         public ViewPage()
         {
             this.InitializeComponent();
+        }
+
+        private async void btnOpenImage_Click(object sender, RoutedEventArgs e)
+        {
+            var picker = new FileOpenPicker();
+            picker.FileTypeFilter.Add(".jpg");
+            picker.SuggestedStartLocation = PickerLocationId.PicturesLibrary;
+
+            var file = await picker.PickSingleFileAsync();
+            if (file != null)
+            {
+                // Stream needs to be disposed by the end of scope to release resources
+                using (IRandomAccessStream fileStream = await file.OpenAsync(Windows.Storage.FileAccessMode.Read))
+                {
+                    BitmapImage bitmapImage = new BitmapImage();
+                    await bitmapImage.SetSourceAsync(fileStream);
+
+                    img.Source = bitmapImage;
+                }
+            }
         }
     }
 }
